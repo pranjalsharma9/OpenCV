545a91639b35b5ae5c66a9724210da8e5929f8e4
pranjalsharma9 <pranjalsharma9@gmail.com>
2017-04-05 01:39:23 +0530

    Camera working partially
    - Adds code to access the device camera.
    - Adds code to request for camera permission.
    - Camera works only on landscape on testing device.

--- app/src/main/java/nsit/pranjals/opencv/MainActivity.java
+++ app/src/main/java/nsit/pranjals/opencv/MainActivity.java
@@ -1,10 +1,43 @@
 package nsit.pranjals.opencv;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.MotionEvent;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener,
+        CameraBridgeViewBase.CvCameraViewListener2 {
+
+    private CameraBridgeViewBase cameraView;
+
+    private Mat rgba;
+
+    private BaseLoaderCallback loaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS :
+                    cameraView.enableView();
+                    cameraView.setOnTouchListener(MainActivity.this);
+                    break;
+                default :
+                    super.onManagerConnected(status);
+                    break;
+            }
+        }
+    };
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -16,14 +49,69 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        init();
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+    private void init () {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA}, 0);
+        }
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        cameraView = (CameraBridgeViewBase) findViewById(R.id.camera_view_act_main);
+        cameraView.setVisibility(SurfaceView.VISIBLE);
+        cameraView.setCvCameraViewListener(this);
+    }
+
+    @Override
+    protected void onResume () {
+        super.onResume();
+        if (!OpenCVLoader.initDebug()) {
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_2_0, this, loaderCallback);
+        } else {
+            loaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
+
+    @Override
+    protected void onPause () {
+        super.onPause();
+        if (cameraView != null) {
+            cameraView.disableView();
+        }
+    }
+
+    @Override
+    protected void onDestroy () {
+        super.onDestroy();
+        if (cameraView != null) {
+            cameraView.disableView();
+        }
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        rgba = new Mat();
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        rgba.release();
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        rgba = inputFrame.rgba();
+        return rgba;
+    }
 }
